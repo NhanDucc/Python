c79f74e21e710d71c9b40982b07ec867ba81049c
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2023-12-21 17:15:17 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,20 @@
+import math
+n = int(input("Enter the number of values: "))
+list = []
+for i in range (1, n+1):
+    value = int(input(f"Enter value {i}: "))
+    list.append(value)
+
+S = 0
+for i in list:
+    S += i
+M = S / n
+print(f"Mean: {M}")
+
+S1 = 0
+for i in list:
+    x = i - M
+    x = x*x
+    S1 += x
+Sd = math.sqrt((1/(n-1))*S1)
+print(f"Standard deviation: {Sd}")
