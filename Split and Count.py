e85e64bfe72ea725750a163f05fe1ac91b82ed1b
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-01-30 19:02:12 +0700

    Update and rename 111111111111.py to Split and Count.py

--- Split and Count.py
+++ Split and Count.py
@@ -11,7 +11,7 @@ def spilit_and_transfer(lst):
             sublist[element].append(element)
     return list(sublist.values())
 
-file = "F:/aaaaaa/text.txt"
+file = "__blank__" #Enter the path to the file to split and count the elements into __blank__
 data = read_file(file)
 sublist = spilit_and_transfer(data)
 for sublists in sublist:
