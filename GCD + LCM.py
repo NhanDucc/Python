120b2a56304afa26e1fabc7be0cd76a1cb22d099
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-05-11 22:09:08 +0700

    Update and rename GCD + SCM.py to GCD + LCM.py

--- GCD + LCM.py
+++ GCD + LCM.py
@@ -5,16 +5,14 @@ def find_gcd(a, b):
         a, b = b, a % b
     return a
 
-def find_scm(a, b):
-    gcd = find_gcd(a, b)
-    scm = (a * b) // gcd
-    return scm
+def find_lcm(a, b):
+    return (a * b) / find_gcd(a, b)
 
 num_1 = int(input('Enter first number: '))
 num_2 = int(input('Enter second number: '))
 
 gcd = find_gcd(num_1, num_2)
-scm = find_scm(num_1, num_2)
+lcm = find_lcm(num_1, num_2)
 
 print("Greatest common divisor of", num_1, "and", num_2, "is", gcd)
-print("Smallest common multiple of", num_1, "and", num_2, "is", scm)
+print("Least common multiple of", num_1, "and", num_2, "is", lcm)
