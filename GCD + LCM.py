80281724d2176fd90ba177463ff61e23691a4a22
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-05-11 22:11:51 +0700

    Update GCD + LCM.py

--- GCD + LCM.py
+++ GCD + LCM.py
@@ -6,13 +6,11 @@ def find_gcd(a, b):
     return a
 
 def find_lcm(a, b):
-    return (a * b) / find_gcd(a, b)
+    return (a * b) // find_gcd(a, b)
 
-num_1 = int(input('Enter first number: '))
-num_2 = int(input('Enter second number: '))
-
-gcd = find_gcd(num_1, num_2)
-lcm = find_lcm(num_1, num_2)
-
-print("Greatest common divisor of", num_1, "and", num_2, "is", gcd)
-print("Least common multiple of", num_1, "and", num_2, "is", lcm)
+if __name__ == '__main__':
+    num_1 = int(input('Enter first number: '))
+    num_2 = int(input('Enter second number: '))
+    
+    print("Greatest common divisor of", num_1, "and", num_2, "is", find_gcd(num_1, num_2))
+    print("Least common multiple of", num_1, "and", num_2, "is", find_lcm(num_1, num_2))
