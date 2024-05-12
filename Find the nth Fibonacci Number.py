b61ea95ea2a95abc2cf91d464ad81a1a9c0b9184
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-05-12 21:31:38 +0700

    Delete Find the nth Fibonacci Number.py

--- Find the nth Fibonacci Number.py
+++
@@ -1,6 +0,0 @@
-import math
-
-n = int(input('Enter the ordinal number of the Fibonacci number to find: '))
-fibo = round(((1 + math.sqrt(5))**n - (1 - math.sqrt(5))**n)/(2**n * math.sqrt(5)))
-
-print(f'Fibonacci number {n} is: {fibo}')
