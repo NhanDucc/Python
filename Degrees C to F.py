232ffc1958f0d9a9bd1342db136e10b7edeeb167
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-05-12 12:01:11 +0700

    Delete Degrees C to F.py

--- Degrees C to F.py
+++
@@ -1,6 +0,0 @@
-C = int(input('Enter the temperature in degrees C: '))
-if 0 <= C <= 10**5:
-    F = (C*9/5) + 32
-    print('%.2f' %F)
-else:
-    print('Please re-enter the temperature in degrees C')
