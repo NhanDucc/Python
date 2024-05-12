406868a37e9e54243306354c038b347d00286863
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-05-12 11:52:25 +0700

    Delete Basic Math/Factorial directory

--- Basic Math/Factorial/Factorial.py
+++
@@ -1,8 +0,0 @@
-def calculate_factorical(n):        # math.factorial
-    if n == 0:
-        return 1
-    else:
-        return n*calculate_factorical(n-1)
-
-n = int(input('Enter the number to calculate the factorial: '))
-print(f'{n}! = {calculate_factorical(n)}')
