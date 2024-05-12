64e0f4684a241b1cc95fc0ace77dbef153b879c2
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-05-12 11:37:56 +0700

    Add files via upload

---
+++ Divisor and Multiple/GCD and LCM.py
@@ -0,0 +1,18 @@
+def find_gcd(a, b):        # math.gcd
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def find_lcm(a, b):        # math.lcm
+    return (a * b) // find_gcd(a, b)
+
+if __name__ == '__main__':
+    num_1 = int(input('Enter first number: '))
+    num_2 = int(input('Enter second number: '))
+    
+    print("Greatest common divisor of", num_1, "and", num_2, "is", find_gcd(num_1, num_2))
+    print("Least common multiple of", num_1, "and", num_2, "is", find_lcm(num_1, num_2))
+
+    # a, b, c = 10, 20, 35
+    # print(find_gcd(find_gcd(a, b), c))
+    # print(find_lcm(find_lcm(a, b), c))
