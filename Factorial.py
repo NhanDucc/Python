ea5e37b33d3d5cda3b1fac012032c1ef1ef9abd4
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-05-12 11:51:07 +0700

    Delete Factorial.py

--- Factorial.py
+++
@@ -1,9 +0,0 @@
-def calculate_factorical(n):
-    if n == 0:
-        return 1
-    else:
-        return n*calculate_factorical(n-1)
-
-n = int(input('Enter the number to calculate the factorial: '))
-factorical = calculate_factorical(n)
-print(f'{n}! = {factorical}')
